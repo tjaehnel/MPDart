1351fa2567bb18413995be424b08dc68aeeb9ee1
Tobias Jaehnel <tjaehnel@gmail.com>
2015-08-03 22:31:01 +0200

    Minor renaming.

--- covers.php
+++ covers.php
@@ -7,32 +7,32 @@ require_once('getid3/getid3.php');
 $isValidRequest = false;
 if(substr($coverfile, -9) == "cover.jpg")
 {
-	$coverfile = substr($coverfile, 0, -9);
+	$coverdir = substr($coverfile, 0, -9);
 	$isValidRequest = true;
 }
 if(substr($coverfile, -10) == "folder.jpg")
 {
-	$coverfile = substr($coverfile, 0, -10);
+	$coverdir = substr($coverfile, 0, -10);
 	$isValidRequest = true;
 }
 
 
 if($isValidRequest)
 {
-	if($handle = opendir($coverfile))
+	if($handle = opendir($coverdir))
 	{
 		// Iterate through all files in the directory
 		// until we find a music file that includes a cover
 		while (false !== ($entry = readdir($handle)))
 		{
-			$mp3file = $coverfile."/".$entry;
+			$mp3file = $coverdir."/".$entry;
 			if(is_dir($mp3file))
 				continue;
 			// Fetch image from ID3 information
 			$getID3 = new getID3;
-			$OldThisFileInfo = $getID3->analyze($mp3file);
-			if(isset($OldThisFileInfo['comments']['picture'][0])){
-	   			$Image='data:'.$OldThisFileInfo['comments']['picture'][0]['image_mime'].';charset=utf-8;base64,'.base64_encode($OldThisFileInfo['comments']['picture'][0]['data']);
+			$id3Information = $getID3->analyze($mp3file);
+			if(isset($id3Information['comments']['picture'][0])){
+	   			$Image='data:'.$id3Information['comments']['picture'][0]['image_mime'].';charset=utf-8;base64,'.base64_encode($id3Information['comments']['picture'][0]['data']);
 				break;
 			}
 		}
@@ -40,7 +40,7 @@ if($isValidRequest)
 	}
 	// send the cover image to the browser
 	header('Content-Type: image/jpeg');
-	echo $OldThisFileInfo['comments']['picture']['0']['data'];
+	echo $id3Information['comments']['picture']['0']['data'];
 }
 else
 {
